f1a07c82f8492fb6ccf6b6a16d60c4a9b214c637
Diwakar <sdiwakar2005s@gmail.com>
2025-09-09 20:48:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,12 @@ with app.app_context():
     db.create_all()
 
 # -------------------- Routes --------------------
+
+@app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
 
 # -------- Lost (List only) --------
 @app.route("/lost")
